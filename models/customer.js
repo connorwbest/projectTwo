a5f714dc12a50f6d691450e7e24565e72b61f92e
Gabriel Ferrer <gabrielferrer@mail.com>
2018-08-25 19:54:27 -0400

    Update customer.js
    matched model file to updated salesdb erd

--- models/customer.js
+++ models/customer.js
@@ -1,22 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
   var Customer = sequelize.define("Customer", {
-    name: {
+    firstname: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    business_name: {
+    lastname: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    business_address: {
+    address: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
     },
-    lead_status: {
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
       type: DataTypes.STRING,
       allowNull: false
     }
 
   });
+  Customer.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Customer.hasMany(models.Followup, {
+      onDelete: "cascade"
+    });
+  };
+
   return Customer;
 };
