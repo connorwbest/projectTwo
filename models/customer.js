0a698687d40105a51fbd450e7c54af7866fb9bdf
Gabriel Ferrer <gabrielferrer@mail.com>
2018-08-25 20:28:09 -0400

    Update customer.js

--- models/customer.js
+++ models/customer.js
@@ -23,8 +23,6 @@ module.exports = function(sequelize, DataTypes) {
 
   });
   Customer.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
     Customer.hasMany(models.Followup, {
       onDelete: "cascade"
     });
